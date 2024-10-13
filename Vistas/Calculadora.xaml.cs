b144407d96182c0def47d27125ec21a8181f300c
MOISES GUZMAN MORA <moisomor22@gmail.com>
2024-10-13 15:26:03 -0600

    Botones

--- Vistas/Calculadora.xaml.cs
+++ Vistas/Calculadora.xaml.cs
@@ -23,5 +23,10 @@ namespace CalculadoraWPF.Vistas
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
